3770e4c3600ac1dd5ad36b55e8fa2122e85c1304
Zong-Ying Lyu <zongyinglyu@dopod.com>
2014-08-05 11:33:55 +0800

    1.把 io 搬回 router.js

--- router.js
+++ router.js
@@ -1,10 +1,46 @@
 var async = require('async');
-module.exports = function(app) {
+module.exports = function(app, io) {
+	var users = [];
+    var rooms = [];
+    
     //router
     app.get('/hello', function(req, res){
         res.send('Hello World');
     });
     
+    io.on('connection', function(socket) {
+        console.log('a user connected');
+
+        var nickname = undefined;
+        var room = undefined;
+        
+        socket.on('join', function(msg) {            
+            console.log(msg);
+            socket.join(msg.room);            
+            room = msg.room;
+            nickname = msg.nickname;
+
+            //users.push(msg.nickname);
+            users[socket.id] = {
+                name: msg.nickname
+            }
+
+            io.to(room).emit('message', nickname + " join");
+        });
+
+        socket.on('message', function(msg) {
+            console.log(msg);
+            console.log(socket.rooms);
+            //console.log(socket.client);
+            io.to(room).emit('message', nickname + '：' + msg.message);
+            console.log(users);
+        });
+
+        socket.on('disconnect', function() {
+            console.log('user disconnected');
+        });
+    });
+
     app.get('*', notFound);
 };
 
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ exports.start = function (config) {
     var app = express();
     var http = require('http').Server(app);
     var io = require('socket.io')(http);
-    var users = [];
-    var rooms = [];
     // need redis server
     // var redis = require('socket.io-redis');
     // io.adapter(redis({ host: 'localhost', port: 6379 }));    
@@ -26,41 +24,8 @@ exports.start = function (config) {
     // });
 
     app.use(express.static( __dirname + '/public'));
-
-    io.on('connection', function(socket) {
-        console.log('a user connected');
-
-        var nickname = undefined;
-        var room = undefined;
-        
-        socket.on('join', function(msg) {            
-            console.log(msg);
-            socket.join(msg.room);            
-            room = msg.room;
-            nickname = msg.nickname;
-
-            //users.push(msg.nickname);
-            users[socket.id] = {
-                name: msg.nickname
-            }
-
-            io.to(room).emit('message', nickname + " join");
-        });
-
-        socket.on('message', function(msg) {
-            console.log(msg);
-            console.log(socket.rooms);
-            //console.log(socket.client);
-            io.to(room).emit('message', nickname + '：' + msg.message);
-            console.log(users);
-        });
-
-        socket.on('disconnect', function() {
-            console.log('user disconnected');
-        });
-    });
     
-    //require('./router')(app);
+    require('./router')(app, io);
 
     var server = http.listen(config.port, function() {
         console.log('Listening on port %d', server.address().port);
